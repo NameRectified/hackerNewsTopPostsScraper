2de5b02e95780ea619efb4d6878b49e664ae6e69
Jakka Balaji Mahendra <85291879+NameRectified@users.noreply.github.com>
2025-03-20 18:41:58 +0530

    working version for single page

--- main.py
+++ main.py
@@ -7,33 +7,22 @@ import requests
 from bs4 import BeautifulSoup
 import pprint # for better printing
 
+
+
 response = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(response.text,'html.parser')
 
-# for title in soup.find_all('span',class_="titleline"):
-#     for link in title.find_all('a'):
-#         print(link.get('href'),link.text,sep='\n',end='\n\n')
-#     # print(title.find_all('a').get('href'),end="\n\n")
-
-# for elem in soup.find_all('tr',class_='athing'):
-    # print(elem.get('id'),end='\n\n')
-
+def sort_stories(dict):
+    return sorted(dict,key=lambda key:key['votes'],reverse=True)
 posts = []
 for score_elem in  soup.find_all('span',class_='score'):
-    # print(score_elem)
-    score =int(score_elem.text.split(' ')[0]) # tyou can use .replace(' points','') like on the website the score is stored as eg 100 points and replace points with empty string and then convert to integer
-    if score>100:
-        # print(score,score_elem.get('id').split('_')[1])
+    score =int(score_elem.text.split(' ')[0]) # you can use .replace(' points','') like on the website the score is stored as eg 100 points and replace points with empty string and then convert to integer
+    if score>=100:
         print(score)
         id_ = int(score_elem.get('id').split('_')[1])
         row_with_score_greater_than_100 = soup.find(id=id_)
         for titleline in row_with_score_greater_than_100.find_all('span',class_="titleline"):
             for link in titleline.find_all('a',recursive=False): # we want only the direct anchor children and not all anchor tags
-            # for link in titleline.select('a:first-of-type'):
-                # print(link)
-                # print(link.get('href'),link.text,sep='\n',end='\n\n')
                 posts.append({'title':link.text,'link':link.get('href',None),'votes':score})
-                # posts.append({'title':link.text,'link':link.get('href',None)})
-                # posts.append({title:link.text})
-    pprint.pprint(posts)
-            # print(title.find_all('a').get('href'),end="\n\n")
+    # pprint.pprint(posts)
+    pprint.pprint(sort_stories(posts))
