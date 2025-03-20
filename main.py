099e63b362ade1fb6794b20b217f5a3ba0d4ae6e
Jakka Balaji Mahendra <85291879+NameRectified@users.noreply.github.com>
2025-03-20 18:48:05 +0530

    working multi page scraper

--- main.py
+++ main.py
@@ -7,22 +7,27 @@ import requests
 from bs4 import BeautifulSoup
 import pprint # for better printing
 
-
-
-response = requests.get('https://news.ycombinator.com/')
-soup = BeautifulSoup(response.text,'html.parser')
-
 def sort_stories(dict):
     return sorted(dict,key=lambda key:key['votes'],reverse=True)
-posts = []
-for score_elem in  soup.find_all('span',class_='score'):
-    score =int(score_elem.text.split(' ')[0]) # you can use .replace(' points','') like on the website the score is stored as eg 100 points and replace points with empty string and then convert to integer
-    if score>=100:
-        print(score)
-        id_ = int(score_elem.get('id').split('_')[1])
-        row_with_score_greater_than_100 = soup.find(id=id_)
-        for titleline in row_with_score_greater_than_100.find_all('span',class_="titleline"):
-            for link in titleline.find_all('a',recursive=False): # we want only the direct anchor children and not all anchor tags
-                posts.append({'title':link.text,'link':link.get('href',None),'votes':score})
-    # pprint.pprint(posts)
-    pprint.pprint(sort_stories(posts))
+def story_scraper():
+    posts = []
+    for score_elem in  soup.find_all('span',class_='score'):
+        score =int(score_elem.text.split(' ')[0]) # you can use .replace(' points','') like on the website the score is stored as eg 100 points and replace points with empty string and then convert to integer
+        if score>=100:
+            print(score)
+            id_ = int(score_elem.get('id').split('_')[1])
+            row_with_score_greater_than_100 = soup.find(id=id_)
+            for titleline in row_with_score_greater_than_100.find_all('span',class_="titleline"):
+                for link in titleline.find_all('a',recursive=False): # we want only the direct anchor children and not all anchor tags
+                    posts.append({'title':link.text,'link':link.get('href',None),'votes':score})
+        # pprint.pprint(posts)
+        pprint.pprint(sort_stories(posts))
+
+
+# page_count = int(input("Enter the number of pages you wish to scrape:"))
+page_count = 5
+for count in range(1,page_count+1):
+    response = requests.get(f"https://news.ycombinator.com/?p={count}")
+    soup = BeautifulSoup(response.text,'html.parser')
+    print(f"This is for page {count} ")
+    story_scraper()
